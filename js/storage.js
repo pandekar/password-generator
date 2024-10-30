7ecd3d97c63c6079e9fac17388bec1c73738ac2c
pandekar <pandeadhistanaya@gmail.com>
2024-10-30 21:16:25 +0700

    save last 5 generated passwords

--- js/storage.js
+++ js/storage.js
@@ -1,9 +1,35 @@
+const STORAGE_KEY = 'LAST-GENERATED-PASSWORD';
+
+const getStorageItem = () => JSON.parse(localStorage.getItem(STORAGE_KEY));
+
 export function savePassword(password, url) {
-  localStorage.setItem('lastGeneratedPassword', JSON.stringify({ password, url }));
+  const data = getStorageItem();
+
+  if (data !== null) {
+    if (data.length > 4) {
+      // removes the first element
+      data.shift();
+      // adds the specified elements to the end of an array
+      data.push({ password, url });
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    } else {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify([...data, { password, url }]));
+    }
+  } else {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([{ password, url }]));
+  }
 };
 
 export function getLastPassword() {
-  const storedPassword = localStorage.getItem('lastGeneratedPassword');
+  const storedPassword = localStorage.getItem(STORAGE_KEY);
+  let lastPassword = null;
 
-  return storedPassword ? JSON.parse(storedPassword) : null;
+  const parsedData = JSON.parse(storedPassword);
+  if (storedPassword) {
+    lastPassword = parsedData[parsedData.length - 1];
+
+    return lastPassword;
+  }
+
+  return null;
 };
